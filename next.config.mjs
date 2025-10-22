0585216edfa9085e8126503c63c636c29b338e5b
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2025-10-22 18:19:44 +0600

    Deploy Fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/army-camp-location", // must match your repo name
+  assetPrefix: "/army-camp-location/", // ensures public assets (JSON, images) resolve
+  reactStrictMode: true,
+};
 
 export default nextConfig;
